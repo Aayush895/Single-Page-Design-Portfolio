50538496c990ac795d0928a08fedf59563a694b8
Aayush895 <aayush895@gmail.com>
2023-12-05 23:11:02 +0530

    Changes in the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,6 @@ function App() {
       <Work />
       <Consultation />
       <Footer />
-      {/* 
-
-      - Header --> Done
-        - Navbar --> Done
-        - Header-body --> Done
-      - About --> Done
-      - Work Section --> In progress
-        - Work / Custom slider 
-      - Consultation (Aside section) --> Done
-      - Footer --> Done
-
- 
-
-   */}
     </main>
   )
 }
